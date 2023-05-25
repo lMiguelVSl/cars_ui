9af06132e9b66d6cbb0a47d97d4ca778303f9b36
Miguel <mavargass@coreo.iue.edu.co>
2023-05-24 22:34:45 -0400

    application structure

---
+++ src/app/components/main-view/main-view.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-main-view',
+    templateUrl: './main-view.component.html'
+})
+export class MainViewComponent {
+
+}
---
+++ src/app/services/car-services.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CarService {
+    constructor(private http: HttpClient) {}
+
+    getCars() {
+        return this.http.get(``);
+    }
+}
